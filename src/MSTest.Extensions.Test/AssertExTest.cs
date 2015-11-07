7682b2f6851456704736119fa92a64b942373667
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2015-11-07 11:59:05 +0900

    AssertEx.Throws実装

---
+++ src/MSTest.Extensions.Test/AssertExTest.cs
@@ -0,0 +1,71 @@
+﻿namespace MSTest.Extensions.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MSTest.Extensions;
+
+    /// <summary>
+    /// AssertEx テストクラス
+    /// </summary>
+    [TestClass]
+    public class AssertExTest
+    {
+        /// <summary>
+        /// AssertEx.Throws 
+        /// 正常系
+        /// </summary>
+        [TestMethod]
+        public void ThrowsTest1()
+        {
+            Action action = () => { throw new ApplicationException("Test"); };
+            var ex = AssertEx.Throws<ApplicationException>(action);
+            
+            Assert.AreEqual("Test", ex.Message);
+        }
+
+        /// <summary>
+        /// AssertEx.Throws 
+        /// 例外が発生しないケース
+        /// </summary>
+        [TestMethod]
+        public void ThrowsTest2()
+        {
+            try
+            {
+                Action action = () => { };
+                AssertEx.Throws<ApplicationException>(action);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(AssertFailedException));
+                Assert.AreEqual("AssertEx.Throws に失敗しました。例外が発生しません。", ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// AssertEx.Throws 
+        /// 検証対象の例外の型が異なる
+        /// </summary>
+        [TestMethod]
+        public void ThrowsTest3()
+        {
+            try
+            {
+                Action action = () => 
+                {
+                    int num1 = 1;
+                    int num2 = 0;
+                    int num3 = num1 / num2;
+                };
+
+                AssertEx.Throws<ApplicationException>(action);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(AssertFailedException));
+                Assert.AreEqual(string.Format("AssertEx.Throws に失敗しました。<{0}>が必要ですが、<{1}>が指定されました。", typeof(ApplicationException).FullName, typeof(DivideByZeroException).FullName), ex.Message);
+            }
+        }
+        
+    }
+}
---
+++ src/MSTest.Extensions/AssertEx.cs
@@ -0,0 +1,44 @@
+﻿namespace MSTest.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Assert拡張クラス
+    /// </summary>
+    public static class AssertEx
+    {
+
+        /// <summary>
+        /// 指定したメソッドの実行時に指定した例外が発生することを確認します。<br />
+        /// 例外が発生しない場合、または指定した例外と異なる例外が発生した場合、アサーションは失敗します。
+        /// </summary>
+        /// <typeparam name="T">検証対象の例外の型</typeparam>
+        /// <param name="action">検証対象のメソッド</param>
+        /// <returns>検証対象の例外</returns>
+        public static T Throws<T>(Action action) where T : System.Exception
+        {
+            try
+            {
+                action();
+                throw new AssertFailedException("AssertEx.Throws に失敗しました。例外が発生しません。");
+            }
+            catch (AssertFailedException)
+            {
+                throw;
+            }
+            catch (System.Exception ex)
+            {
+                if (ex is T)
+                    return ex as T;
+                else
+                    throw new AssertFailedException(string.Format("AssertEx.Throws に失敗しました。<{0}>が必要ですが、<{1}>が指定されました。", typeof(T).FullName, ex.GetType().FullName));
+            }
+        }
+
+    }
+}
